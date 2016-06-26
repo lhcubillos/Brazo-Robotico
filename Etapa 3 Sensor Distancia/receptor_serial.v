e71ab47d9e391cd3fdd99e4d07d66d1cc73a1824
Luis Hernán Cubillos <lhcubillos@uc.cl>
2016-06-26 15:22:04 -0500

    Starting serial receptor

---
+++ Etapa 3 Sensor Distancia/receptor_serial.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:16:31 06/26/2016 
+// Design Name: 
+// Module Name:    receptor_serial 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module receptor_serial(
+		input clk,
+		input canal_serial,
+		
+		output reg [7:0] angulo_servo_1,
+		output reg [7:0] angulo_servo_2,
+		output reg [7:0] angulo_servo_3,
+		output reg [7:0] angulo_servo_4
+		
+    );
+	 
+	 
+	 
+	 
+	 
+
+
+endmodule
